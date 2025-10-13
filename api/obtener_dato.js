5e21d268a30aa34300e054709ee3d91a09fff93e
NTorresX <ernesto2001.ceti@gmail.com>
2025-10-12 23:01:30 -0600

    Update obtener_dato.js

--- api/obtener_dato.js
+++ api/obtener_dato.js
@@ -2,11 +2,10 @@ import { kv } from '@vercel/kv';
 
 export default async function handler(request, response) {
   try {
-    // Obtener el valor de la base de datos
-    const glucosa = await kv.get('glucosa_actual');
+    // OBTIENE el primer elemento de la lista (el más reciente)
+    const glucosaReciente = await kv.lindex('historial_glucosa', 0);
     
-    // Responder con el dato en formato JSON
-    return response.status(200).json({ glucosa: glucosa || '---' });
+    return response.status(200).json({ glucosa: glucosaReciente || '---' });
 
   } catch (error) {
     return response.status(500).json({ error: error.message });
