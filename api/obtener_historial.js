f47d78d6eec3bbe0c727435d98a8baef6cebe34e
NTorresX <ernesto2001.ceti@gmail.com>
2025-11-12 23:32:57 -0600

    Update obtener_historial.js

--- api/obtener_historial.js
+++ api/obtener_historial.js
@@ -2,18 +2,16 @@ import { kv } from '@vercel/kv';
 
 export default async function handler(request, response) {
   try {
-    // OBTIENE todos los elementos (que ahora son strings JSON)
-    const stringHistorial = await kv.lrange('historial_glucosa', 0, -1);
+    // OBTIENE todos los elementos (que ahora son solo números)
+    const historial = await kv.lrange('historial_glucosa', 0, -1);
     
-    // Parsea cada string JSON para convertirlo en un objeto
-    const parsedHistorial = (stringHistorial || []).map(item => JSON.parse(item));
-    
-    // Devuelve el historial como un array de objetos
-    return response.status(200).json({ historial: parsedHistorial || [] });
+    // Devuelve el historial como un array de números
+    return response.status(200).json({ historial: historial || [] });
 
   } catch (error) {
     return response.status(500).json({ error: error.message });
   }
 }
+
 
 
