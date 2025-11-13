90614f48d80550b31b54193361e4cd6ee3882c1b
NTorresX <ernesto2001.ceti@gmail.com>
2025-11-12 21:34:17 -0600

    Update obtener_paciente.js

--- api/obtener_paciente.js
+++ api/obtener_paciente.js
@@ -6,7 +6,7 @@ export default async function handler(request, response) {
     const nombre = await kv.get('paciente_nombre');
     
     // Devuelve el nombre o un valor por defecto si no se ha configurado
-    return response.status(200).json({ nombre: nombre || 'Paciente no asignado' });
+    return response.status(200).json({ "nombre": nombre});
 
   } catch (error) {
     return response.status(500).json({ error: error.message });
